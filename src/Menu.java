bb3d4a5092a54af753f85a7897dca8c6d8ae677f
Anas <anassemghouni774@gmail.com>
2024-12-11 15:16:46 +0100

    Premiere versions du menu

---
+++ src/Menu.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Menu {
+
+    public static void MenuOthello() {
+        Scanner scanner  = new Scanner(System.in);
+
+        int choix = 0;
+
+        System.out.println("BUT DU JEU :");
+        System.out.println("Avoir plus de pions de sa couleur que l’adversaire à la fin de la partie. Celle-ci s’achève lorsque aucun des deux joueurs ne peut plus jouer de coup légal. " + ("\n") +
+                "Cela intervient généralement lorsque les 64 cases sont occupées.");
+
+        System.out.println(("\n") + "1. Jouer contre l'ordinateur.");
+        System.out.println("2. Jouer contre un joueur.");
+
+        System.out.println(("\n") + "Choisissez votre mode de jeu (Tapez 1 ou 2) :");
+        choix = scanner.nextInt();
+
+        switch (choix) {
+            case 1:
+                // Pour jouer a deux
+                break;
+
+            case 2:
+                // Pour joueur avec une IA
+                break;
+        }
+    }
+}
