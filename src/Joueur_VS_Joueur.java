5b17bdbd689bcf03518bfe6001c860eaf64be16c
Anas <anassemghouni774@gmail.com>
2024-12-16 11:53:01 +0100

    Début boucle de jeu + Ajout de methodes

--- src/Joueur_VS_Joueur.java
+++ src/Joueur_VS_Joueur.java
@@ -1,4 +1,5 @@
 import javax.swing.plaf.synth.SynthOptionPaneUI;
+import java.util.Scanner;
 
 public class Joueur_VS_Joueur {
 
@@ -7,7 +8,73 @@ public class Joueur_VS_Joueur {
     public static char[][] plateau = new char[LIGNES][COLONNES];
     public static char joueur = 'B';
 
-    public static void boucleDeJeu() {
+    public static void boucleDeJeuJoueurVSJoueur() {
+
+        initialiserPlateau();
+
+        Scanner scanner = new Scanner(System.in);
+
+        boolean enCours = true;
+
+        while (enCours) {
+
+            afficherPlateau();
+
+            System.out.println("\nJoueur " + joueur + ", choisissez une ligne (1-7): ");
+            System.out.println("\nJoueur " + joueur + ", choisissez une colonne (1-7): ");
+
+            int ligne, colonne;
+
+            do {
+                ligne = scanner.nextInt() - 1;
+                colonne = scanner.nextInt() - 1;
+
+                if (ligne < 0 || ligne > LIGNES) {
+                    System.out.println("Veuillez choisir une ligne entre 1 et 7 : ");
+                }
+                else if (colonne < 0 || colonne >= COLONNES) {
+                    System.out.println("Veuillez choisir une colonne entre 1 et 7 : ");
+                }
+            } while (ligne < 0 || ligne >= LIGNES || colonne < 0 || colonne >= COLONNES);
+
+            placerJeton(ligne,colonne);
+
+
+            /*              Le joueur 'B' gagne
+
+            if () {
+
+            }
+
+            */
+
+            /*              Le joueur 'N' à gagné
+
+            else if () {
+
+            }
+
+            */
+
+            /*              Le plateau est rempli
+
+            else if () {
+
+            }
+
+            */
+
+            /*             Aucun retournement possible
+
+            else if () {
+
+            }
+
+             */
+
+            joueur = (joueur == 'N') ? 'B' : 'N';
+
+        }
 
     }
 
@@ -18,7 +85,6 @@ public class Joueur_VS_Joueur {
             }
         }
         QuatrePion();
-
     }
 
     public static void afficherPlateau() {
@@ -42,13 +108,9 @@ public class Joueur_VS_Joueur {
         System.out.println("  1 2 3 4 5 6 7 8");
     }
 
-    public static void placerJeton() {
-        for (int i = 0; i < LIGNES; i++) {
-            for (int j = 0; j < COLONNES; j++) {
-                if (plateau[i][j] == ' ') {
-                    plateau[i][j] = joueur;
-                }
-            }
+    public static void placerJeton(int ligne, int colonne) {
+        if (plateau[ligne][colonne] == ' ') {
+            plateau[ligne][colonne] = joueur;
         }
     }
 
@@ -60,6 +122,15 @@ public class Joueur_VS_Joueur {
         plateau[milieuLignes - 1][milieuColonnes] = 'N';
         plateau[milieuLignes][milieuColonnes - 1] = 'N';
         plateau[milieuLignes][milieuColonnes] = 'B';
+    }
+
+    public static boolean grilleRemplie() {
+        for (int j = 0; j < COLONNES; j++) {
+            if (plateau[0][j] == ' ') {
+                return false;
+            }
+        }
+        return true;
     }
 
     public static void main(String[] args) {
