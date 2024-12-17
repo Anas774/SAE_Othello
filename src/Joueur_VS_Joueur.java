7a03dae7b54729e7195c17d02b265ece64efac0c
Anas <anassemghouni774@gmail.com>
2024-12-17 16:22:24 +0100

    Ajout peutPrendrePion + estDansPlateau

--- src/Joueur_VS_Joueur.java
+++ src/Joueur_VS_Joueur.java
@@ -133,6 +133,44 @@ public class Joueur_VS_Joueur {
         return true;
     }
 
+    public static boolean peutPrendrePion(char[][] plateau, int x, int y, char joueur) {
+
+        if (plateau[x][y] != ' ') {
+            return false; // La case doit être vide pour jouer
+        }
+
+        char adversaire = (joueur == 'N') ? 'B' : 'N';
+
+        // Directions : haut, bas, gauche, droite, et diagonales
+        int[] directionsX = {-1, -1, -1, 0, 0, 1, 1, 1};
+        int[] directionsY = {-1, 0, 1, -1, 1, -1, 0, 1};
+
+        for (int d = 0; d < 8; d++) {
+            int i = x + directionsX[d];
+            int j = y + directionsY[d];
+            boolean trouveAdversaire = false;
+
+            // Parcourir dans une direction donnée
+            while (estDansLePlateau(i, j) && plateau[i][j] == adversaire) {
+                trouveAdversaire = true;
+                i += directionsX[d];
+                j += directionsY[d];
+            }
+
+            // Si un pion de notre couleur est trouvé après des adversaires
+            if (trouveAdversaire && estDansLePlateau(i, j) && plateau[i][j] == joueur) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    // Vérifier si une position est dans le plateau
+    public static boolean estDansLePlateau(int x, int y) {
+        return x >= 0 && x < LIGNES && y >= 0 && y < COLONNES;
+    }
+
     public static void main(String[] args) {
         initialiserPlateau();
 
